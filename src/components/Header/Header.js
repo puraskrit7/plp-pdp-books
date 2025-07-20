ac43c3b95d593473027365b775195e3c9f7e4e09
PURASKRIT <puraskrit@192.168.0.103>
2025-07-20 23:54:05 +0530

    ffirebase deployment

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -37,7 +37,7 @@ const Header = ({ cartCount }) => {
 
   return (
     <header className="header-nav">
-      <span>Bookstore</span>
+      <span>Welcome to Puraskrit's Bookstore</span>
       
       <div className="header-actions">
         <div className="cart-icon">
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 // webpack.config.js
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: './',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: isProduction ? "./" : "/",
     clean: true,
   },
   module: {
@@ -18,43 +18,43 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: "babel-loader",
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html',
-      inject: 'body',
+      template: "./public/index.html",
+      filename: "index.html",
+      inject: "body",
     }),
     new Dotenv(),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'),
+      directory: path.join(__dirname, "public"),
     },
     historyApiFallback: true,
     port: 3000,
     open: true,
     hot: true,
   },
-  mode: isProduction ? 'production' : 'development',
+  mode: isProduction ? "production" : "development",
   performance: {
     hints: false,
   },
